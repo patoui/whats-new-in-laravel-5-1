3cb3ef270e1f22a9a9ad541600530d2cce5356a1
Patrique Ouimet <patrique.ouimet@gmail.com>
2017-02-03 09:40:09 -0500

    Episode 11

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
+// Authentication routes...
+// Route::get('auth/login', 'Auth\AuthController@getLogin');
+// Route::post('auth/login', 'Auth\AuthController@postLogin');
+// Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// // Registration routes...
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 // event(new App\Events\UserHasRegistered);
 
 Route::get('/', function () {
@@ -56,3 +65,6 @@ Route::group(
         );
     }
 );
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
